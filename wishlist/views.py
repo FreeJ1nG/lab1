b333b68a9d792fc7ffd0455ca0a13b4325461009
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-09-07 01:40:10 +0700

    finished lab 1

---
+++ wishlist/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from wishlist.models import BarangWishlist
+
+# Create your views here.
+def show_wishlist(request):
+  return render(
+    request,
+    "wishlist.html",
+    {
+      'list_barang': BarangWishlist.objects.all(),
+      'nama': "Andrew Jeremy"
+    }
+  )
